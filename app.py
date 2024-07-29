c41148f74f22b9effb6dcfd18905655984c93d8c
twishaahuja <98633633+twishaahuja@users.noreply.github.com>
2024-07-29 19:32:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,370 @@
+# import os
+# import numpy as np
+# import speech_recognition as sr
+# import whisper
+# import torch
+
+# from datetime import datetime, timedelta
+# from queue import Queue
+# from time import sleep
+# import streamlit as st
+# from langchain.text_splitter import CharacterTextSplitter
+# from langchain.memory import ConversationBufferMemory
+# from langchain.chains import ConversationalRetrievalChain
+# from htmlTemplates import css, bot_template, user_template
+# from PyPDF2 import PdfReader
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain_google_genai import GoogleGenerativeAIEmbeddings
+# import google.generativeai as genai
+# from langchain_community.vectorstores import FAISS
+# from langchain_google_genai import ChatGoogleGenerativeAI
+# from dotenv import load_dotenv
+# from langchain_community.document_loaders import PyPDFLoader
+
+# load_dotenv()
+# os.getenv("GOOGLE_API_KEY")
+# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+# def get_pdf_text(pdf_docs):
+#     text = ""
+#     for pdf in pdf_docs:
+#         pdf_reader = PdfReader(pdf)
+#         for page in pdf_reader.pages:
+#             text += page.extract_text()
+#     return text
+
+# def get_text_chunks(text):
+#     text_splitter = CharacterTextSplitter(
+#         separator="\n",
+#         chunk_size=1000,
+#         chunk_overlap=200,
+#         length_function=len
+#     )
+#     chunks = text_splitter.split_text(text)
+#     return chunks
+
+# def get_vectorstore(text_chunks):
+#     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+#     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+#     return vectorstore
+
+# def get_conversation_chain(vectorstore):
+#     llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature=0.5)
+#     # llm = ChatGoogleGenerativeAI(model="gemini-pro",
+#     #                              client=genai,
+#     #                              temperature=0.3)
+#     memory = ConversationBufferMemory(
+#         memory_key='chat_history', return_messages=True)
+#     conversation_chain = ConversationalRetrievalChain.from_llm(
+#         llm=llm,
+#         retriever=vectorstore.as_retriever(),
+#         memory=memory
+#     )
+#     return conversation_chain
+
+# def handle_userinput(user_question):
+#     response = st.session_state.conversation.invoke({'question': user_question})
+#     st.session_state.chat_history = response['chat_history']
+
+#     for i, message in enumerate(st.session_state.chat_history):
+#         if i % 2 == 0:
+#             st.write(user_template.replace(
+#                 "{{MSG}}", message.content), unsafe_allow_html=True)
+#         else:
+#             st.write(bot_template.replace(
+#                 "{{MSG}}", message.content), unsafe_allow_html=True)
+
+# # def speech(model_name, non_english, energy_threshold, record_timeout, phrase_timeout):
+# def speech(audio_model, record_timeout, phrase_timeout, energy_threshold):
+#     phrase_time = None
+#     data_queue = Queue()
+#     recorder = sr.Recognizer()
+#     recorder.energy_threshold = energy_threshold
+#     recorder.dynamic_energy_threshold = False
+#     source = sr.Microphone(sample_rate=16000)
+#     # model = f"{model_name}.en" if model_name != "large" and not non_english else model_name
+#     # audio_model = whisper.load_model(model)
+#     transcription = ''
+
+#     with source:
+#         recorder.adjust_for_ambient_noise(source)
+
+#     abort = [False]
+
+#     def record_callback(_, audio: sr.AudioData) -> None:
+#         if abort[0]:
+#             return
+#         data = audio.get_raw_data()
+#         data_queue.put(data)
+
+#     recorder.listen_in_background(source, record_callback, phrase_time_limit=record_timeout)
+    
+#     st.write("Model loaded.\n")
+#     start_time = datetime.utcnow()
+
+#     while True:
+#         now = datetime.utcnow()
+#         if (now - start_time).seconds >= 20:
+#             abort[0] = True
+#             break
+
+#         if not data_queue.empty():
+#             phrase_complete = False
+#             if phrase_time and now - phrase_time > timedelta(seconds=phrase_timeout):
+#                 phrase_complete = True
+#             phrase_time = now
+
+#             audio_data = b''.join(data_queue.queue)
+#             data_queue.queue.clear()
+
+#             audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
+#             result = audio_model.transcribe(audio_np, fp16=torch.cuda.is_available())
+#             text = result['text'].strip()
+
+#             if phrase_complete:
+#                 transcription += " " + text
+#             else:
+#                 transcription = transcription.rstrip() + " " + text
+
+#             os.system('cls' if os.name == 'nt' else 'clear')
+#             # st.write(transcription)
+#             print('', end='', flush=True)
+#         else:
+#             sleep(0.25)
+
+#     # st.write("\n\nTranscription:")
+#     # st.write(transcription)
+#     return transcription
+
+# def main():
+#     st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+#     st.write(css, unsafe_allow_html=True)
+
+#     if "conversation" not in st.session_state:
+#         st.session_state.conversation = None
+#     if "chat_history" not in st.session_state:
+#         st.session_state.chat_history = None
+
+#     st.header("Chat with multiple PDFs :books:")
+
+#     model_name = "medium"
+#     non_english = False 
+#     energy_threshold = 1000
+#     record_timeout = 2.0 
+#     phrase_timeout = 3.0
+#     model = f"{model_name}.en" if model_name != "large" and not non_english else model_name
+#     audio_model = whisper.load_model(model)
+
+#     col1, col2 = st.columns([3, 1])
+
+#     with col1:
+#         user_question = st.text_input("Ask a question about your documents:")
+#     with col2:
+#         if st.button("REC"):
+#             st.write("Starting transcription...")
+#             # user_question = speech(model_name, non_english, energy_threshold, record_timeout, phrase_timeout)
+#             user_question = speech(audio_model, record_timeout, phrase_timeout, energy_threshold)
+#             st.write("Transcription completed:")
+
+#     # user_question = st.text_input("Ask a question about your documents:")
+
+#     # if st.button("REC"):
+#     #     # st.write("Starting transcription...")
+#     #     user_question = speech(model_name, non_english, energy_threshold, record_timeout, phrase_timeout)
+#     #     # st.write("Transcription completed:")
+    
+#     if user_question:
+#         handle_userinput(user_question)
+
+#     with st.sidebar:
+#         st.subheader("Your documents")
+#         pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+#         if st.button("Process"):
+#             with st.spinner("Processing"):
+#                 raw_text = get_pdf_text(pdf_docs)
+#                 text_chunks = get_text_chunks(raw_text)
+#                 vectorstore = get_vectorstore(text_chunks)
+#                 st.success("Done")
+#                 st.session_state.conversation = get_conversation_chain(vectorstore)
+
+# if __name__ == '__main__':
+#     main()
+
+import os
+import numpy as np
+import speech_recognition as sr
+import whisper
+import torch
+
+from datetime import datetime, timedelta
+from queue import Queue
+from time import sleep
+import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemplates import css, bot_template, user_template
+from PyPDF2 import PdfReader
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+    )
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
+    return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature=0.5)
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation.invoke({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
+def speech(audio_model, record_timeout, phrase_timeout, energy_threshold):
+    phrase_time = None
+    data_queue = Queue()
+    recorder = sr.Recognizer()
+    recorder.energy_threshold = energy_threshold
+    recorder.dynamic_energy_threshold = False
+    source = sr.Microphone(sample_rate=16000)
+    transcription = ''
+
+    with source:
+        recorder.adjust_for_ambient_noise(source)
+
+    abort = [False]
+
+    def record_callback(_, audio: sr.AudioData) -> None:
+        if abort[0]:
+            return
+        data = audio.get_raw_data()
+        data_queue.put(data)
+
+    recorder.listen_in_background(source, record_callback, phrase_time_limit=record_timeout)
+    
+    st.markdown("<div style='text-align: center; position: fixed; padding-left: 60px; bottom: 3rem; padding-top:1px;'>Speak now</div>", unsafe_allow_html=True)
+
+    start_time = datetime.utcnow()
+
+    while True:
+        now = datetime.utcnow()
+        if (now - start_time).seconds >= 20:
+            abort[0] = True
+            break
+
+        if not data_queue.empty():
+            phrase_complete = False
+            if phrase_time and now - phrase_time > timedelta(seconds=phrase_timeout):
+                phrase_complete = True
+            phrase_time = now
+
+            audio_data = b''.join(data_queue.queue)
+            data_queue.queue.clear()
+
+            audio_np = np.frombuffer(audio_data, dtype=np.int16).astype(np.float32) / 32768.0
+            result = audio_model.transcribe(audio_np, fp16=torch.cuda.is_available())
+            text = result['text'].strip()
+
+            if phrase_complete:
+                transcription += " " + text
+            else:
+                transcription = transcription.rstrip() + " " + text
+
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print('', end='', flush=True)
+        else:
+            sleep(0.25)
+
+    return transcription
+
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    st.header("Chat with multiple PDFs :books:")
+
+    model_name = "medium"
+    non_english = False 
+    energy_threshold = 1000
+    record_timeout = 2.0 
+    phrase_timeout = 3.0
+    model = f"{model_name}.en" if model_name != "large" and not non_english else model_name
+    audio_model = whisper.load_model(model)
+
+    col1, col2 = st.columns([3, 1])
+
+    with col1:
+        user_question = st.text_input("Ask a question about your documents:")
+    with col2:
+        if st.button("REC"):
+            # st.markdown("<div style='text-align: center; position: fixed; padding-left: 60px; bottom: 3rem; padding-bottom:2px;'>PLease wait...</div>", unsafe_allow_html=True)
+            user_question = speech(audio_model, record_timeout, phrase_timeout, energy_threshold)
+            # st.markdown("<div style='text-align: center; position: fixed; padding-left: 50px; bottom: 3rem; padding-top:1px; padding-bottom:2px;'>Transcription completed:</div>", unsafe_allow_html=True)
+
+    if user_question:
+        handle_userinput(user_question)
+
+    with st.sidebar:
+        st.subheader("Your documents")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text = get_pdf_text(pdf_docs)
+                text_chunks = get_text_chunks(raw_text)
+                vectorstore = get_vectorstore(text_chunks)
+                st.success("Done")
+                st.session_state.conversation = get_conversation_chain(vectorstore)
+
+if __name__ == '__main__':
+    main()
+
